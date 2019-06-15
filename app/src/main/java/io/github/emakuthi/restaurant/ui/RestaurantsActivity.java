50973a392b608581e933e258314b5cea3bee9f0c
developer <emakuthi@gmail.com>
2019-06-15 20:14:44 +0300

    add list adapter for the recycler view

--- app/src/main/java/io/github/emakuthi/restaurant/ui/RestaurantsActivity.java
+++ app/src/main/java/io/github/emakuthi/restaurant/ui/RestaurantsActivity.java
@@ -3,6 +3,8 @@ package io.github.emakuthi.restaurant.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.emakuthi.restaurant.R;
+import io.github.emakuthi.restaurant.adapters.RestaurantListAdapter;
 import io.github.emakuthi.restaurant.models.Restaurant;
 import io.github.emakuthi.restaurant.services.YelpService;
 import okhttp3.Call;
@@ -22,9 +25,9 @@ import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.recyclerView)RecyclerView mRecyclerView;
     public ArrayList<Restaurant> restaurants = new ArrayList<>();
+    private RestaurantListAdapter mAdapter;
 
 
     @Override
@@ -35,7 +38,6 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
         getRestaurants(location);
 
     }
@@ -53,37 +55,19 @@ public class RestaurantsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-                    restaurants = yelpService.processResults(response);
+                restaurants = yelpService.processResults(response);
 
-                    RestaurantsActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
+                RestaurantsActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
 
-                            String[] restaurantNames = new String[restaurants.size()];
-
-                            for(int i = 0; i < restaurantNames.length; i++){
-
-                                restaurantNames[i] = restaurants.get(i).getName();
-                            }
-
-                            ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
-                            mListView.setAdapter(adapter);
-
-                            for (Restaurant restaurant : restaurants){
-                                Log.d(TAG, "Name: " + restaurant.getName());
-                                Log.d(TAG, "Phone: " + restaurant.getPhone());
-                                Log.d(TAG, "Website: " + restaurant.getWebsite());
-                                Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                                Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-                                Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-                                Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-
-
-                            }
-
-                        }
-                    });
-
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), restaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
             }
         });
 
